ba0790aa6af84707ee18b1bb1259a1219b16f38d
IsaGher <sg_0300@hotmail.com>
2021-06-05 10:48:11 -0600

    ultimos ejercicios de serie de Taylor

---
+++ ejercicioDiez.py
@@ -0,0 +1,23 @@
+
+x = float(input("Ingrese valor de x: "))
+cifras = float(input("Ingrese cuantas cifras significativas: "))
+es = 0.5*(10**(2-cifras))
+ea = 1+es
+n = 1
+ant = 1
+
+while ea>es:
+    f = 1
+    for i in range(1,n+1):
+        f += (((-1)**i)*(x**(2*i)))
+    try:
+        ea = round(abs((f-ant)/f)*100,6)
+    except ZeroDivisionError:
+        print("Resultado: x= 0")
+        exit()
+    print('{:^10}{:^10}{:^10}'.format('Iteracion','x','error'))
+    print('{:^10}{:^10}{:^10}'.format(n,f,ea))
+    ant = f
+    n += 1
+
+print("Resultado: x=",f," con error de: ",ea)
